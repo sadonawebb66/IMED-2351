c346dd5a407d0e3ee80085d8ccfef5a4043d1dda
sadonawebb66 <sadonawebb.66@gmail.com>
2023-01-25 16:59:48 -0600

    module 2 assignment
    added some js

--- sadonas.js
+++ sadonas.js
@@ -1,2 +1,14 @@
 console.log('Would you consider yourself more of a DC fan or a Marvel fan?');
 document.write(9+10);
+var scope ="global"; //global variable
+console.log('marvel for me');  //output to console
+const dog={
+    breed:"heeler", color:"red", size:"large"    //block variable and object variable
+};
+dog.color="blue";
+
+let a = 4;
+let x = (9+10) * a;            //arithmetic/ mathematical operations
+document.getElementById("arithmetic").innerHTML = x;
+
+var carName1="Buggy"; //string variable
