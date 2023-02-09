06edab9fddc1a0c4c033112ecc9db221d7826576
sadonawebb66 <sadonawebb.66@gmail.com>
2023-02-08 21:03:50 -0600

    module4
    switch and string methods

--- sadonas.js
+++ sadonas.js
@@ -23,3 +23,42 @@ let text = "I am not funny";
 let part = text.slice(9, 14);
 
 document.getElementById("slice-example").innerHTML = part;
+
+//module 4 work below
+const catName ="Abby";
+const animal ="cat";
+document.getElementById("cat-description").innerHTML =      //concatenated variable
+`I have a ${animal} and her name is ${catName} and I love her so much.`;
+let marrital ="married";
+if (marrital = "married"){
+  console.log(`Way to go! You're not single!`);     //conditional statement
+} else if (marrital ="dating"){
+  console.log(`You're not lame, I'm happy for you.`);
+}else{
+  console.log(`You're lame, go touch some grass and maybe you'll find someone.`);
+}
+let fruits = "Apple";
+switch(fruits) {
+  case "Banana":
+    alert("Hello")
+    break;                    //switch statement
+  case "Apple":
+    alert("Welcome")
+    break;
+  
+default:
+
+    alert("Neither");
+}
+document.getElementById("fruits").innerHTML = "alert";      //string method
+function myFunction() {
+  let text = document.getElementById("animal-switch").innerHTML;
+  document.getElementById("animal-switch").innerHTML =
+  text.replace("dog Jax","cat Abby");}
+
+  let p = 9.656;
+  document.getElementById("number-method").innerHTML = 
+    p.toPrecision() + "<br>" +
+    p.toPrecision(16) + "<br>" +                    //number method
+    p.toPrecision(4) + "<br>" +
+    p.toPrecision(8);  
